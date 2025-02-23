31dddeacd192566c19e0ced028fc7a686b0aa58b
Dan Willoughby <amozoss@gmail.com>
2025-02-22 21:21:56 -0700

    host and port

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,10 +56,17 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :chore_chart, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
+  secret_key_base =
+    System.get_env("CHORE_CHART_SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  host = System.get_env("CHORE_CHART_PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("CHORE_CHART_PHX_PORT") || "4000")
 
   config :chore_chart, ChoreChartWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
